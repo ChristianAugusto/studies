d26711db680e48709fe9e86716c9e98dc89ac0a3
Christian Augusto <christianbetta@gmail.com>
2019-12-22 14:22:38 -0300

    Adding webpack course

---
+++ Courses/React/webpack/babel/src/assets/js/home.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import hello from "./modules/home/index";
+
+ReactDOM.render(hello, document.querySelector(".test"));
---
+++ Courses/React/webpack/babel/src/assets/js/teste.js
@@ -0,0 +1,10 @@
+import "../css/style.css";
+import "../scss/style.scss"
+
+const funcao = () => {
+
+  console.log("Teste");
+
+};
+
+funcao();
