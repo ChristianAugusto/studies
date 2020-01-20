a6fd4d3dc90f715f9934cd937c808b41559a5f1b
ChristianAugusto <christianbetta@gmail.com>
2020-01-20 18:33:10 -0300

    Initing calculator-react-course

---
+++ Courses/React/calculator-machine-course/src/components/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import styled from "styled-components";
+
+import AppStyle from "./App.style";
+
+const AppWrapper = styled.div`${AppStyle}`;
+
+export default props => (
+  <AppWrapper>
+    
+  </AppWrapper>
+);
