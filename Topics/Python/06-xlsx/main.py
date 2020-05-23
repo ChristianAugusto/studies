f80c8e12c5f029b23c3adb4eb13ccca9e5dd2aab
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2020-05-22 22:42:07 -0300

    (Python) Adicionando xlsx modulo

---
+++ Topics/Python/06-xlsx/main.py
@@ -0,0 +1,22 @@
+from openpyxl import load_workbook
+
+
+wb = load_workbook('./mercado hortifruit.xlsx')
+
+# len(wb.worksheets) # quantidade de planilhas no arquivo
+
+# for index, sheet in enumerate(wb.worksheets):
+#     print(f'Sheet Index:[{index}], Title: {sheet.title}')
+
+# for i in range(0, len(wb.worksheets)):
+#     print(f'Sheet Index: {i}, Title: {wb.worksheets[i].title}')
+
+# for sheet in wb.worksheets:
+#     print(sheet.title)
+
+
+worksheet = wb.worksheets[0]
+
+for row_cells in worksheet.iter_rows():
+    for cell in row_cells:
+        print(f'{cell}: cell.value={cell.value}')
