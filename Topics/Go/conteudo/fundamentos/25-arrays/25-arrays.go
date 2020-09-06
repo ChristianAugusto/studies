7b311532f16186f7fbce37326224ba1a13450d0c
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2020-09-06 14:34:55 -0300

    (Go) Adicionando for em array

--- Topics/Go/conteudo/fundamentos/25-arrays/25-arrays.go
+++ Topics/Go/conteudo/fundamentos/25-arrays/25-arrays.go
@@ -8,7 +8,7 @@ func main() {
 	// Arrays em go são fixos e homogêneos
 
 	var x [5]int
-	y := [5]int{1, 2, 3, 4, 5}
+	y := [...]int{1, 2, 3, 4, 5} // compilador conta
 
 	x[0] = 2
 
@@ -18,4 +18,21 @@ func main() {
 	fmt.Printf("%T\n", x)
 
 	fmt.Println("len(x) =", len(x))
+
+	var notas [3]float64
+	total := 0.0
+
+	notas[0], notas[1], notas[2] = 7.8, 4.3, 9.1
+
+	// for i := 0; i < len(notas); i++ {
+	// 	total += notas[i]
+	// }
+
+	for _, numero := range notas {
+		total += numero
+	}
+
+	media := total / float64(len(notas))
+
+	fmt.Println(media)
 }
