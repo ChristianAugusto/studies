fb68f3a46ba671c8b873787ca2ecc117b5f50f2b
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2020-09-07 14:18:53 -0300

    (criptografia-front-back) Adicionando fluxo

---
+++ Topics/Projetos-Estudo/criptografia-front-back/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const JSEncrypt = require('node-jsencrypt');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const jsonParser = bodyParser.json();
+// const textParser = bodyParser.text();
+const jsEncrypt = new JSEncrypt({default_key_size: 2056});
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve('./public/index.html'));
+});
+
+app.post('/', jsonParser, function(req, res) {
+    console.log('req.body', req.body);
+
+    const privateKey = fs.readFileSync(path.resolve('./keys/privatekey.pem'), 'utf8');
+
+    jsEncrypt.setKey(privateKey);
+
+    Object.entries(req.body).forEach(([key, value]) => req.body[key] = jsEncrypt.decrypt(value))
+
+    res.send(JSON.stringify(req.body));
+});
+
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
