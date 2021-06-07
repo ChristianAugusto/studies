08ce616e4cbfb68803dd3e975dbce5f0d441c9d9
Christian Augusto Martins dos Santos Barros <christian.barros@enjoei.com.br>
2021-06-07 15:38:19 -0300

    feat(Ruby): Adicionando mais métodos padrões rails no crud

--- Topics/Ruby/02-ruby-on-rails/crudpostgresql2/app/controllers/person_controller.rb
+++ Topics/Ruby/02-ruby-on-rails/crudpostgresql2/app/controllers/person_controller.rb
@@ -1,83 +1,47 @@
 class PersonController < ApplicationController
+  before_action :set_user, only: [:get_by_id, :update, :delete]
+
+  # normalmente esse metodo chamace index
   def get
-    limit = Integer(ENV['API_PEOPLE_LIMIT'])
-
-    if params['limit'] != nil
-      aux = Integer(params['limit'])
-
-      if aux <= Integer(ENV['API_PEOPLE_LIMIT'])
-        limit = aux
-      end
-    end
-
-    offset = params['offset'] != nil ? Integer(params[:offset]) : 0
-
+    limit =  params['limit'].to_i <= ENV['API_PEOPLE_LIMIT'].to_i ? params['limit'].to_i : ENV['API_PEOPLE_LIMIT'].to_i
+    offset = params['offset'] != nil ? params[:offset].to_i : 0
     people = Person.all.limit(limit).offset(offset).order('id ASC')
-
-    render :json => people
+    render json: people
   end
 
+  # normalmente esse metodo chamace show
   def get_by_id
-    id = Integer(params[:id])
+    render json: @user == nil ? {} : @user
+  end
 
-    user = Person.find_by(id: id)
-
-    render :json => user == nil ? {} : user
+  # normalmente esse metodo chamace create
+  def post
+    @person = Person.new(person_params)
+    if @person.save
+      render json: @person
+    else
+      render json: person.errors
+    end
   end
 
   def update
-    id = Integer(params[:id])
-
-    user = Person.find_by(id: id)
-
-    request_body = JSON.parse(request.body.read)
-
-    if request_body['name'] != nil
-      user.name = request_body['name']
-    end
-
-    if request_body['email'] != nil
-      user.email = request_body['email']
-    end
-
-    if request_body['cpf'] != nil
-      user.cpf = request_body['cpf']
-    end
-
-    if request_body['rg'] != nil
-      user.rg = request_body['rg']
-    end
-
-    user.save
-
-    render :json => user
+    @user.update(person_params)
+    render json: @user
   end
 
-  def post
-    request_body = JSON.parse(request.body.read)
-
-    request_body['name'] = request_body['name'].gsub("'", "")
-    request_body['email'] = request_body['email'].gsub("'", "")
-    request_body['cpf'] = request_body['cpf'].gsub("'", "")
-    request_body['rg'] = request_body['rg'].gsub("'", "")
-
-    person = Person.new
-    person.name = request_body['name']
-    person.email = request_body['email']
-    person.cpf = request_body['cpf']
-    person.rg = request_body['rg']
-
-    person.save
-
-    render :json => {message: 'Success'}
-  end
-
+  # normalmente esse metodo chamace delete
   def delete
-    id = Integer(params[:id])
+    @user.destroy
+    render json: {message: 'Success'}
+  end
 
-    user = Person.find_by(id: id)
-    user.destroy
+  private
 
-    render :json => {message: 'Success'}
+  def person_params
+    params.require(:person).permit(:name, :email, :cpf, :rg)
+  end
+
+  def set_user
+    @user = Person.find(params[:id])
   end
 end
