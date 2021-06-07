ca3f51b66dc579dd65fa20fee14131008b70e252
Christian Augusto Martins dos Santos Barros <christian.barros@enjoei.com.br>
2021-06-07 18:45:19 -0300

    feat(Ruby): Iniciando nova playlist da linguagem

--- Topics/Ruby/01-fundamentos/02-types/main.rb
+++ Topics/Ruby/01-fundamentos/02-types/main.rb
@@ -1,21 +1,12 @@
-# Integer, Float, Boolean, String, Array, Symbol, Hash
-
-inteiro = 120
-float = 120.5
-string1 = 'Hello World'
-string2 = "Hello World"
-bool1 = true
-bool2 = false
-array = ['position_0', 'position_1', 'position_2']
-symbol = :simbolo
-hash = {course: 'ruby', language: 'pt-BR', locale: 'onebitcode.com'}
-
-puts inteiro.class
-puts float.class
-puts string1.class
-puts string2.class
-puts bool1.class
-puts bool2.class
-puts array.class
-puts symbol.class
-puts hash.class
+h = {"hash?" => "yep, it\'s a hash!", "the answer to everything" => 42, :linux => "fun for coders."}
+puts "Stringy string McString!".class
+puts 1.class
+puts 1.class.superclass
+puts 1.class.superclass.superclass
+puts 4.3.class
+puts 4.3.class.superclass
+puts nil.class
+puts h.class
+puts :symbol.class
+puts [].class
+puts (1..8).class
