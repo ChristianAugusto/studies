c2dcf0159ac2280deb82f473c13760c98716e72c
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2021-08-23 21:33:56 -0300

    feat(processamento-de-imagens): Adicionando seção

---
+++ topics/processamento-de-imagens/shell-scripts/clean-docker.sh
@@ -0,0 +1,3 @@
+docker container rm -f processamento-de-imagens
+docker image rm -f processamento-de-imagens:latest
+docker network rm rascunho_default
---
+++ topics/processamento-de-imagens/shell-scripts/get-into-container.sh
@@ -0,0 +1,1 @@
+docker exec -it processamento-de-imagens /bin/sh
