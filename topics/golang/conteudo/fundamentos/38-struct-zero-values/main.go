8ffbf807bfa5096207e0ec91f64e6d640d8a8f7d
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2021-11-26 22:15:13 -0300

    golang: Adicionando seção struct valores zero

---
+++ topics/golang/conteudo/fundamentos/38-struct-zero-values/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Foo struct{
+    A string
+}
+
+func main() {
+    fmt.Println(Foo{})
+    fmt.Println(Foo{A:"a"})
+
+    fmt.Println(Foo{} == Foo{})
+    fmt.Printf("Foo{A:\"a\"} == Foo{A:\"b\"} = %v\n", Foo{A:"a"} == Foo{A:"b"})
+    fmt.Println(&Foo{} == &Foo{})
+
+    if (Foo{}) == (Foo{}) {
+        fmt.Println(true)
+    } else {
+        fmt.Println(true)
+    }
+
+    fmt.Println("----------------")
+
+    foo := &Foo{}
+    foo2 := foo
+
+    fmt.Println(foo == foo2)
+}
