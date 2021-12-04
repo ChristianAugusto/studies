ca656537a4abf969ff6d9c9f34667fa546ac8e3e
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2021-12-04 02:11:26 -0300

    lua: Starting with love2d

--- topics/Lua/conteudo/fundamentos/02-types/main.lua
+++
@@ -1,7 +0,0 @@
--- nil, boolean, number, string, function, userdata, thread e table
-
-print(nil)
-print(2)
-print(2.5)
-print("Texto 1")
-print('Texto 2')
---
+++ topics/lua/conteudo/fundamentos/02-types/main.lua
@@ -0,0 +1,10 @@
+-- nil, boolean, number, string, function, userdata, thread e table
+
+print(type("What is my type"))
+t = 10
+
+print(type(5.8*t))
+print(type(true))
+print(type(print))
+print(type(nil))
+print(type(type(ABC)))
